94bf38f2e71b3f20344b35b39640712272adbfe8
Joel Denning <joeldenning@gmail.com>
2020-09-21 10:39:25 -0600

    Using single-spa-web-server-utils

--- server/index-html.js
+++ server/index-html.js
@@ -1,93 +1,61 @@
 import { app } from "./app.js";
-import { importMapPromise } from "./importmap.js";
-import stream from "stream";
 import {
   constructServerLayout,
   setResponseHeaders,
   renderServerResponseBody,
 } from "single-spa-layout/server";
-import merge2 from "merge2";
 import _ from "lodash";
-import { getOverridesFromCookies, applyOverrides } from "import-map-overrides";
+import { getImportMaps } from "single-spa-web-server-utils";
 
 const serverLayout = constructServerLayout({
   filePath: "server/views/index.html",
 });
 
-app.use("*", (req, res) => {
-  global.nodeLoader.setImportMapPromise(
-    importMapPromise.then((map) => {
-      const clone = _.cloneDeep(map);
-
-      const finalMap = applyOverrides(clone, getOverridesFromCookies(req));
-
-      Object.keys(finalMap.imports).forEach((key) => {
-        if (!key.startsWith("@isomorphic-mf")) {
-          delete finalMap.imports[key];
-        }
-      });
-
-      // const finalMap = clone
-      console.log("node", finalMap);
-      return finalMap;
-    })
-  );
-
-  const { bodyStream, applicationProps } = renderServerResponseBody(
-    serverLayout,
-    {
-      urlPath: req.path,
-      renderFragment(name) {
-        const fragStream = new stream.Readable({
-          read() {},
-        });
-        console.log("frag");
-        importMapPromise.then((importMap) => {
-          const browserImportMap = applyOverrides(
-            importMap,
-            getOverridesFromCookies(req)
-          );
-          fragStream.push(
-            `<script type="systemjs-importmap">${JSON.stringify(
-              browserImportMap,
-              null,
-              2
-            )}</script>`
-          );
-          fragStream.push(null);
-        });
-        return fragStream;
-      },
-      renderApplication(props) {
-        if (props.name) {
-          const appStream = merge2();
-          const importSpecifier = props.name + `/server.mjs?ts=${Date.now()}`;
-
-          import(importSpecifier).then((app) => {
-            const byteStream = app.serverRender(props);
-            appStream.add(byteStream);
-          });
-
-          return appStream;
-        } else {
-          return "";
-        }
-      },
-    }
-  );
-
-  setResponseHeaders({
-    res,
-    applicationProps,
-    retrieveApplicationHeaders(props) {
-      return {
-        "x-www-joel": "value",
-      };
+app.use("*", (req, res, next) => {
+  getImportMaps({
+    url:
+      "https://storage.googleapis.com/isomorphic.microfrontends.app/importmap.json",
+    nodeKeyFilter(importMapKey) {
+      return importMapKey.startsWith("@isomorphic-mf");
     },
-    mergeHeaders(headers) {
-      return headers.length > 0 ? headers[0] : {};
-    },
-  }).then(() => {
-    bodyStream.pipe(res);
+    req,
+    allowOverrides: true,
+  }).then(({ nodeImportMap, browserImportMap }) => {
+    global.nodeLoader.setImportMapPromise(Promise.resolve(nodeImportMap));
+
+    const { bodyStream, applicationProps } = renderServerResponseBody(
+      serverLayout,
+      {
+        urlPath: req.path,
+        renderFragment(name) {
+          return `<script type="systemjs-importmap">${JSON.stringify(
+            browserImportMap,
+            null,
+            2
+          )}</script>`;
+        },
+        renderApplication(props) {
+          return import(
+            props.name + `/server.mjs?ts=${Date.now()}`
+          ).then((app) => app.serverRender(props));
+        },
+      }
+    );
+
+    setResponseHeaders({
+      res,
+      applicationProps,
+      retrieveApplicationHeaders(props) {
+        return {
+          "x-www-joel": "value",
+        };
+      },
+      mergeHeaders(headers) {
+        return headers.length > 0 ? headers[0] : {};
+      },
+    }).then(() => {
+      bodyStream.pipe(res);
+      next();
+    });
   });
 });
