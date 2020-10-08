51f1ac54266f559e55e037b24a897ee0372ecb8c
Tarun Kumar Sukhu <tksukhu@gmail.com>
2020-10-08 09:21:25 +0530

    mui experiment

--- server/index-html.js
+++ server/index-html.js
@@ -15,8 +15,7 @@ app.use("*", (req, res, next) => {
   const importSuffix = developmentMode ? `?ts=${Date.now()}` : "";
 
   const importMapsPromise = getImportMaps({
-    url:
-      "https://storage.googleapis.com/isomorphic.microfrontends.app/importmap.json",
+    url: "http://localhost:7000/import-config",
     nodeKeyFilter(importMapKey) {
       return importMapKey.startsWith("@isomorphic-mf");
     },
@@ -40,12 +39,8 @@ app.use("*", (req, res, next) => {
     },
   };
 
-  sendLayoutHTTPResponse({
-    serverLayout,
-    urlPath: req.path,
-    res,
-
-    async renderFragment(name) {
+  const fragments = {
+    importmap: async () => {
       const { browserImportMap } = await importMapsPromise;
       return `<script type="systemjs-importmap">${JSON.stringify(
         browserImportMap,
@@ -53,13 +48,33 @@ app.use("*", (req, res, next) => {
         2
       )}</script>`;
     },
+    "global-styles": async () => {
+      await importMapsPromise;
+      const app = await import(
+        `@isomorphic-mf/navbar` + `/server.mjs${importSuffix}`
+      );
+      return `<style id="jss-server-side">
+              ${app.serverRender(props).sheets}
+              </style>
+              `;
+    },
+  };
+
+  const renderFragment = (name) => fragments[name]();
+
+  sendLayoutHTTPResponse({
+    serverLayout,
+    urlPath: req.path,
+    res,
+    renderFragment,
     async renderApplication({ appName, propsPromise }) {
       await importMapsPromise;
+      console.log(`APPNAME = ${appName}`);
       const [app, props] = await Promise.all([
         import(appName + `/server.mjs${importSuffix}`),
         propsPromise,
       ]);
-      return app.serverRender(props);
+      return app.serverRender(props).htmlStream;
     },
     async retrieveApplicationHeaders({ appName, propsPromise }) {
       await importMapsPromise;
